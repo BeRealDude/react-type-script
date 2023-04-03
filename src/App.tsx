876f881ab9897da09e9477613cdfdd9e2ac5eaa3
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-04-03 19:55:04 +0300

    feat: get response

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+import React, { useState, useEffect } from "react";
+import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
+import axios from "axios";
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
 
-  const users: IUser[] = [
-    {id: 1, name: 'Vlad', email: 'vlad@vlad.ru', address: {city: 'St.Peterburg', street: 'Mira 6', zipcode: '123' }},
-    {id: 2, name: 'Misha', email: 'misha@misha.ru', address: {city: 'St.Peterburg', street: 'Zvezdnay', zipcode: '123' }}
-  ]
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+      setUsers(response.data)
+    } catch (e) {
+      alert(e);
+    }
+  }
 
   return (
     <div>
-      <Card onClick={ () => { console.log('функция работает') } } variant={CardVariant.primary} width='200px' height='200px'>
+      <Card
+        onClick={() => {
+          console.log("функция работает");
+        }}
+        variant={CardVariant.primary}
+        width="200px"
+        height="200px"
+      >
         <button>Кнопка</button>
       </Card>
       <UserList users={users} />
     </div>
   );
-}
+};
 
 export default App;
