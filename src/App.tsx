8bf443436fa1ef3f934d06a7fc52f32525a4dfd7
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-04-03 18:58:38 +0300

    feat: UserList, types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+
+  const users: IUser[] = [
+    {id: 1, name: 'Vlad', email: 'vlad@vlad.ru', address: {city: 'St.Peterburg', street: 'Mira 6', zipcode: '123' }},
+    {id: 2, name: 'Misha', email: 'misha@misha.ru', address: {city: 'St.Peterburg', street: 'Zvezdnay', zipcode: '123' }}
+  ]
+
   return (
     <div>
       <Card onClick={ () => { console.log('функция работает') } } variant={CardVariant.primary} width='200px' height='200px'>
         <button>Кнопка</button>
       </Card>
+      <UserList users={users} />
     </div>
   );
 }
